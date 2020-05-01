1cbd8972f34be7dfb89c25c8d4162001ac88d754
green5555 <47073816+green5555@users.noreply.github.com>
2020-05-01 11:32:51 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define xx first
+#define yy second
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+}
