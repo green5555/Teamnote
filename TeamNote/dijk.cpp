323d12ab34670712b694d4c5401c51438f5f94f8
Green Jung <47073816+green5555@users.noreply.github.com>
2020-07-22 09:23:43 +0900

    Update dijk.cpp

--- TeamNote/dijk.cpp
+++ TeamNote/dijk.cpp
@@ -1,21 +1,20 @@
-const int MAX, INF;
-vector<int> dijkstra(int start, auto &adj){	
-	vector<int> dist(MAX, INF);
-	dist[start] = 0;
-	priority_queue <pii,vector<pii>,greater<pii>> Q;
-	Q.push({0,start});
-	while(!Q.empty()){
-		int cost = Q.top().first;
-		int here = Q.top().second;
-		Q.pop();
-		if(cost > dist[here]) continue;
-		for(auto &there : adj[here]){
-			int newCost = cost + there.first;
-			if(dist[there.second] > newCost){
-				Q.push({newCost, there.second});
-				dist[there.second] = newCost;
-			}
-		}
-	}
-	return dist;
+const int MAX; const ll INF;
+vector<pll> adj[MAX];
+void dijkstra(int start, vector<ll> &dist){
+    dist = vector<ll>(MAX, INF);
+    dist[start] = 0;
+    priority_queue<pll,vector<pll>,greater<pll>> Q;
+    Q.push({0,start});
+    while(!Q.empty()){
+        ll cost = Q.top().xx, here = Q.top().yy;
+        Q.pop();
+        if(cost > dist[here]) continue;
+        for(auto &there : adj[here]){
+            ll newCost = cost + there.xx;
+            if(dist[there.yy] > newCost){
+                Q.push({newCost, there.yy});
+                dist[there.yy] = newCost;
+            }
+        }
+    }
 }
