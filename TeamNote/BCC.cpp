94f66c05aa25c104eacc058084e1cebbf2532fa3
Green Jung <47073816+green5555@users.noreply.github.com>
2020-07-22 09:27:56 +0900

    Update BCC.cpp

--- TeamNote/BCC.cpp
+++ TeamNote/BCC.cpp
@@ -1,54 +1,54 @@
-const int MAXN = 100;
-struct BCC{
-	vector<pair<int, int>> graph[MAXN]; // { next vertex id, edge id }
-	int up[MAXN], visit[MAXN], vtime;
-	vector<int> stk;
-	int is_cut[MAXN]; // v is cut vertex if is_cut[v] > 0
-	vector<int> bridge; // list of edge ids
-	vector<int> bcc_edges[MAXN]; // list of edge ids in a bcc
-	int bcc_cnt;
-	void dfs(int nod, int par_edge) {
-		up[nod] = visit[nod] = ++vtime;
-		int child = 0;
-		for (const auto& e : graph[nod]) {
-			int next = e.first, eid = e.second;
-			if (eid == par_edge) continue;
-			if (visit[next] == 0) {
-				stk.push_back(eid);
-				++child;
-				dfs(next, eid);
-				if (up[next] == visit[next]) bridge.push_back(eid);
-				if (up[next] >= visit[nod]) {
-					++bcc_cnt;
-					do {
-						auto lasteid = stk.back();
-						stk.pop_back();
-						bcc_edges[bcc_cnt].push_back(lasteid);
-						if (lasteid == eid) break;
-					} while (!stk.empty());
-					is_cut[nod]++;
-				}
-				up[nod] = min(up[nod], up[next]);
-			}
-			else if (visit[next] < visit[nod]) {
-				stk.push_back(eid);
-				up[nod] = min(up[nod], visit[next]);
-			}
-		}
-		if (par_edge == -1 && is_cut[nod] == 1)
-			is_cut[nod] = 0;
-	}
-	// find BCCs & cut vertexs & bridges in undirected graph O(V+E)
-	void get_bcc() {
-		vtime = 0;
-		memset(visit, 0, sizeof(visit));
-		memset(is_cut, 0, sizeof(is_cut));
-		bridge.clear();
-		for (int i = 0; i < n; ++i) bcc_edges[i].clear();
-			bcc_cnt = 0;
-		for (int i = 0; i < n; ++i) {
-			if (visit[i] == 0)
-				dfs(i, -1);
-		}
-	}
-}
+const int MAX = 100;
+struct{
+    vector<pii> graph[MAX]; // { next vertex id, edge id }
+    int up[MAX], visit[MAX], vtime;
+    vector<int> stk;
+    int is_cut[MAX]; // v is cut vertex if is_cut[v] > 0
+    vector<int> bridge; // list of edge ids
+    vector<int> bcc_edges[MAX]; // list of edge ids in a bcc
+    int bcc_cnt;
+    void dfs(int no, int pe) { // node, parent edge
+        up[no] = visit[no] = ++vtime;
+        int child = 0;
+        for (const auto& e : graph[no]) {
+            int next = e.xx, eid = e.yy;
+            if (eid == pe) continue;
+            if (visit[next] == 0) {
+                stk.push_back(eid);
+                ++child;
+                dfs(next, eid);
+                if (up[next] == visit[next]) bridge.push_back(eid);
+                if (up[next] >= visit[no]) {
+                    ++bcc_cnt;
+                    do {
+                        auto lasteid = stk.back();
+                        stk.pop_back();
+                        bcc_edges[bcc_cnt].push_back(lasteid);
+                        if (lasteid == eid) break;
+                    } while (!stk.empty());
+                    is_cut[no]++;
+                }
+                up[no] = min(up[no], up[next]);
+            }
+            else if (visit[next] < visit[no]) {
+                stk.push_back(eid);
+                up[no] = min(up[no], visit[next]);
+            }
+        }
+        if (pe == -1 && is_cut[no] == 1)
+            is_cut[no] = 0;
+    }
+    // find BCCs & cut vertexs & bridges in undirected graph O(V+E)
+    void get_bcc() {
+        vtime = 0;
+        memset(visit, 0, sizeof(visit));
+        memset(is_cut, 0, sizeof(is_cut));
+        bridge.clear();
+        for (int i = 0; i < MAX; ++i) bcc_edges[i].clear();
+        bcc_cnt = 0;
+        for (int i = 0; i < MAX; ++i) {
+            if (visit[i] == 0)
+                dfs(i, -1);
+        }
+    }
+}BCC;
