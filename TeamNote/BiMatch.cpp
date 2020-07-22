21c949bd64bd7e8f0e3a7082711cfc0a73919f6b
Green Jung <47073816+green5555@users.noreply.github.com>
2020-07-22 09:29:18 +0900

    Update BiMatch.cpp

--- TeamNote/BiMatch.cpp
+++ TeamNote/BiMatch.cpp
@@ -3,24 +3,23 @@ int A[MAXA], B[MAXB];
 bool visit[MAXA];
 vector<int> adj[MAXA];
 bool DFS(int here){
-	if(visit[here]) return false;
-	visit[here] = true;
-	for(int &there : adj[here])
-		if(B[there] == -1 || DFS(B[there])){
-			A[here] = there, B[there] = here;
-			return true;
-		}
-	return false;
+    if(visit[here]) return false;
+    visit[here] = true;
+    for(int &there : adj[here])
+        if(B[there] == -1 || DFS(B[there])){
+            A[here] = there, B[there] = here;
+            return true;
+        }
+    return false;
 }
 int biMatch(){
-	memset(A, -1, sizeof(A));
-	memset(B, -1, sizeof(B));
-	int match = 0;
-	for(int i=0; i<MAXA; ++i)
-		if(A[i] == -1){
-			memset(visit, 0, sizeof(visit));
-			if(DFS(i))
-				++match;
-		}
-	return match;
+    memset(A, -1, sizeof(A));
+    memset(B, -1, sizeof(B));
+    int match = 0;
+    for(int i=0; i<MAXA; ++i)
+        if(A[i] == -1){
+            memset(visit, 0, sizeof(visit));
+            match += DFS(i);
+        }
+    return match;
 }
