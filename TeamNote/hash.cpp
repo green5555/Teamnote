1a341d41a8bb28ad43b16f60ce8d4f389315c9a0
Green Jung <47073816+green5555@users.noreply.github.com>
2020-07-30 10:01:27 +0900

    Update hash.cpp

--- TeamNote/hash.cpp
+++ TeamNote/hash.cpp
@@ -1,24 +1,25 @@
+const int MAX = 1000010;
 struct Hashing{
-    const ll base=257, m=1e9+9; //random base : 0.3m ~ 0.7m?
-    int sz;
-    vector<ll> ppow, h;
-    Hashing(const string &s){
-        sz = s.size();
-        ppow.resize(sz);
+    ll base, m;
+    vector<ll> ppow;
+    Hashing(ll _b = 257, ll _m = 1e9+9) : base(_b), m(_m){
+        ppow.resize(MAX);
         ppow[0] = 1;
-        for(int i=1; i<sz; ++i)
+        for(int i=1; i<MAX; ++i)
             ppow[i] = (ppow[i - 1] * base) % m;
-        h.resize(sz+1);
-        for(int i=0; i<sz; ++i)
+    }
+    void hash(vector<ll> &h, string &s){
+        h.clear(); h.resize(s.size()+1);
+        for(int i=0; i<s.size(); ++i)
             h[i+1] = (h[i] + (s[i]+1) * ppow[i]) % m;
     }
-    inline ll get(int l, int len){
+    inline ll get(vector<ll> &h, int l, int len){
         return (h[l+len] - h[l] + m) % m;
     }
-    bool compare(int l1, int len, Hashing &h2, int l2){
-        ll v1 = get(l1, len), v2 = h2.get(l2, len);
+    bool compare(vector<ll> &h1, int l1, vector<ll> &h2, int l2, int len){
+        ll v1 = get(h1, l1, len), v2 = get(h2, l2, len);
         if(l1 < l2) v1 = (v1 * ppow[l2 - l1]) % m;
         if(l1 > l2) v2 = (v2 * ppow[l1 - l2]) % m;
         return v1 == v2;
     }
-};
+}H;
