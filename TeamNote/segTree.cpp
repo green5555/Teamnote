eaf91b5fea4f1c44fe2a3acd66589f65ce1a5741
Green Jung <47073816+green5555@users.noreply.github.com>
2020-08-05 12:21:45 +0900

    Update segTree.cpp

--- TeamNote/segTree.cpp
+++ TeamNote/segTree.cpp
@@ -9,7 +9,7 @@ struct SegmentTree{
     }
     ll update(int i, ll v, int no, int nl, int nr){
         if(i<nl || nr<i) return seg[no];
-        if(nl == nr) return seg[no] = min(seg[no], v);
+        if(nl == nr) return seg[no] = v; // += v;
         int mid = (nl + nr)/2;
         return seg[no] = update(i,v,no*2,nl,mid) + update(i,v,no*2+1,mid+1,nr);
     }
