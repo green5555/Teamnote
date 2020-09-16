1e0400ed553c38a0296d8f7735073c0127d51223
Green Jung <47073816+green5555@users.noreply.github.com>
2020-09-17 04:56:53 +0900

    Update SPFA.cpp

--- TeamNote/SPFA.cpp
+++ TeamNote/SPFA.cpp
@@ -1,27 +1,25 @@
-vector<int> SPFA(int n, int start, auto &adj){
-	vector<int> dist(n, INF);
-	queue<int> Q;
-	bool inQ[MAX] = {};
-	int visit[MAX] = {};
-	dist[start] = 0;
-	Q.push(start);
-	inQ[start] = true;
-	++visit[start];
-	while(!Q.empty()){
-		int here = Q.front();
-		Q.pop(); inQ[here] = false;
-		for(auto &P : adj[here]){
-			int there = P.first, newDist = dist[here] + P.second;
-			if(newDist < dist[there]){
-				dist[there] = newDist;
-				if(!inQ[there]){
-					Q.push(there);
-					inQ[there] = true;
-					if(++visit[there] >= n)
-						return vector<int>();
-				}
-			}
-		}
-	}
-	return dist;
+bool SPFA(int n, int start, vector<int> &dist){
+    dist = vector<int>(n, INF);
+    queue<int> Q;
+    bool inQ[MAX] = {};
+    int visit[MAX] = {};
+    dist[start] = 0;
+    Q.push(start);
+    inQ[start] = true;
+    ++visit[start];
+    while(!Q.empty()){
+        int here = Q.front();
+        Q.pop(); inQ[here] = false;
+        for(auto &P : adj[here]){
+            int there = P.first, newDist = dist[here] + P.second;
+            if(newDist < dist[there]){
+                dist[there] = newDist;
+                if(!inQ[there]){
+                    Q.push(there);
+                    inQ[there] = true;
+                    if(++visit[there] >= n) return false;
+                }
+            }
+        }
+    } return true;
 }
