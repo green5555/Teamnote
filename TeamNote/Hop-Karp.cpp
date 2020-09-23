a0df035e0157014fac2c9a05336a4a4e775be658
Green Jung <47073816+green5555@users.noreply.github.com>
2020-09-24 02:22:14 +0900

    Update Hop-Karp.cpp

--- TeamNote/Hop-Karp.cpp
+++ TeamNote/Hop-Karp.cpp
@@ -48,3 +48,52 @@ int Hopcroft_Karp() {
     }
     return match;
 }
+
+//**************************************
+//tourist's O(VE) faster than hopcroft
+const int MAX = 100;
+struct BiMatching {
+    vector<int> adj[MAX];
+    int iter, pa[MAX], pb[MAX], was[MAX];
+
+    bool dfs(int v) {
+        was[v] = iter;
+        for (int u : adj[v]) {
+            if (pb[u] == -1) {
+                pa[v] = u;
+                pb[u] = v;
+                return true;
+            }
+        }
+        for (int u : adj[v]) {
+            if (was[pb[u]] != iter && dfs(pb[u])) {
+                pa[v] = u;
+                pb[u] = v;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int biMatch() {
+        memset(pa, -1, sizeof(pa));
+        memset(pb, -1, sizeof(pb));
+        memset(was, 0, sizeof(was));
+        iter = 0;
+        int res = 0;
+        while (true) {
+            iter++;
+            int add = 0;
+            for (int i = 0; i < MAX; i++) {
+                if (pa[i] == -1 && dfs(i)) {
+                    add++;
+                }
+            }
+            if (add == 0) {
+                break;
+            }
+            res += add;
+        }
+        return res;
+    }
+}
