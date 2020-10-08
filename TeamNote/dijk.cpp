29e7405341198984a505f6a68c13d92360225bb8
Green Jung <47073816+green5555@users.noreply.github.com>
2020-10-08 17:27:18 +0900

    Update dijk.cpp

--- TeamNote/dijk.cpp
+++ TeamNote/dijk.cpp
@@ -1,19 +1,18 @@
 const int MAX; const ll INF;
-vector<pll> adj[MAX];
+vector<pll> adj[MAX]; //{weight, vertex}
 void dijkstra(int start, vector<ll> &dist){
     dist = vector<ll>(MAX, INF);
     dist[start] = 0;
-    priority_queue<pll,vector<pll>,greater<pll>> Q;
-    Q.push({0,start});
-    while(!Q.empty()){
-        ll cost = Q.top().xx, here = Q.top().yy;
-        Q.pop();
-        if(cost > dist[here]) continue;
-        for(auto &there : adj[here]){
-            ll newCost = cost + there.xx;
-            if(dist[there.yy] > newCost){
-                Q.push({newCost, there.yy});
-                dist[there.yy] = newCost;
+    priority_queue<pll,vector<pll>,greater<pll>> q;
+    q.emplace(0,start);
+    while(!q.empty()){
+    	auto [cost, u] = q.top(); q.pop();
+        if(cost > dist[u]) continue;
+        for(auto &[w, v] : adj[u]){
+            ll ncost = cost + w;
+            if(dist[v] > ncost){
+                q.emplace(ncost, v);
+                dist[v] = ncost;
             }
         }
     }
