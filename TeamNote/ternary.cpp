3d2be8e376a6fbc79be09b8c9b6c10f41ab1a48c
Green Jung <47073816+green5555@users.noreply.github.com>
2020-10-26 06:03:36 +0900

    Update ternary.cpp

--- TeamNote/ternary.cpp
+++ TeamNote/ternary.cpp
@@ -1,22 +1,11 @@
-double ternary (double S, double E) {
-    for(int i=0;i<100;i++) {
-        double L = (2*S+E)/3, R = (S+2*E)/3;
-        if(f(L) < f(R)) S = L;
-        else E = R;
-    }
-    return value(S);
+//아래로 볼록 (최소값): >, 위로 볼록 (최대값) : <
+ll l=0, r=n-1;
+while(l+3<=r) {
+    ll x = (2*l+r)/3, y = (l+2*r)/3;
+    if(f(x) > f(y)) l = x; 
+    else r = y;
 }
+ll ans = INT_MAX;
+for(int i=l; i<=r; ++i) ans = min(ans, f(i));
 
-//대안 : 기울기로 이분 탐색
-int ternary (int S, int E) {
-    int ret = 0x7fffffff;
-    while(S+3<E) {
-        int L = (2*S+E)/3, R = (S+2*E)/3;
-        if(f(L) < f(R)) S = L;
-        else E = R;
-    }
-    for(int i=S;i<=E;i++) {
-        ret = min(ret, f(i));
-    }
-    return ret;
-}
+//실수는 while 대신 for(int i=0;i<100;i++)
