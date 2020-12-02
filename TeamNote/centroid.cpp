fab894518756d5d511d93a368aaf950fc21288a9
Green Jung <47073816+green5555@users.noreply.github.com>
2020-12-03 02:49:13 +0900

    Update centroid.cpp

--- TeamNote/centroid.cpp
+++ TeamNote/centroid.cpp
@@ -1,22 +1,33 @@
+const int MAX;
 bool visit[MAX]; //i를 센트로이드로 문제를 푼 적이 있는가?
-vector<int> adj[MAX]; int sz[MAX];
+vector<int> adj[MAX];
+int sz[MAX];
+
 int get_size(int u, int p) {
     sz[u] = 1;
     for (auto &v : adj[u]) {
         if (v == p || visit[v]) continue;
         sz[u] += get_size(v, u);
-    } return sz[u];
+    }
+    return sz[u];
 }
+
 int get_cen(int u, int p, int cap) { //크기가 cap인 서브트리의 cen
     for (auto &v : adj[u]) {
         if (v == p || visit[v] || sz[v] <= cap / 2) continue;
         return get_cen(v, u, cap);
-    } return u;
+    }
+    return u;
 }
-int solve(int u){
+
+int solve(int u) {
     int cen = get_cen(u, u, get_size(u, u)), ans = INF;
-    for(auto &v : adj[cen]){/*TODO*/} //cen을 지나는 경로들을 처리
-    for(auto &v : adj[cen]){ //분할&정복 : 자식들을 호출
-        if(visit[v]) continue; ans = min(ans, solve(v));
-    } return ans;
+    for (auto &v : adj[cen]) { //cen을 지나는 경로들을 처리
+        //TODO
+    }
+    for (auto &v : adj[cen]) { //분할&정복 : 자식들을 호출
+        if (visit[v]) continue;
+        ans = min(ans, solve(v));
+    }
+    return ans;
 }
