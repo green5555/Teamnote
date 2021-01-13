3daf61a3aa74acbdfac9b2c1416f4ab17d309668
Green Jung <47073816+green5555@users.noreply.github.com>
2021-01-14 04:09:32 +0900

    Update python.py

--- TeamNote/python.py
+++ TeamNote/python.py
@@ -1,6 +1,7 @@
+input = __import__('sys').stdin.readline
+MIS = lambda: map(int, input().split())
+
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 a,b,c = map(int, input().rstrip().split())
-
-input = __import__('sys').stdin.readline
