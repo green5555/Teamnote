109e5662aaf4e713555aeeab517a0e3d3012f239
Green Jung <47073816+green5555@users.noreply.github.com>
2021-01-24 04:21:50 +0900

    Update dncopt.cpp

--- TeamNote/dncopt.cpp
+++ TeamNote/dncopt.cpp
@@ -5,7 +5,7 @@ void solve_dnc(int i, int l, int r, int optl, int optr){
     int mid = (l + r)/2;
     pll best = {LLONG_MAX, -1};
     for (int k = optl; k <= min(mid, optr); k++)
-        best = min(best, {dp[i-1][k] + C(k, mid), k});
+        best = min(best, {dp[i-1][k] + C(k+1, mid), k});
     dp[i][mid] = best.first;
     solve_dnc(i, l, mid - 1, optl, best.second);
     solve_dnc(i, mid + 1, r, best.second, optr);
