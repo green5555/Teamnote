27fa8be510afd8a66654ac4167702f20542a428b
Green Jung <47073816+green5555@users.noreply.github.com>
2021-02-06 11:44:17 +0900

    Update deque_rmq.cpp

--- TeamNote/deque_rmq.cpp
+++ TeamNote/deque_rmq.cpp
@@ -1,3 +1,39 @@
+struct DequeMin{
+    deque<pii> q;
+    int i=0, j=0;
+
+    void push(int x){
+        while(!q.empty() && q.back().xx >= x) q.pop_back();
+        q.push_back({x, j});
+        ++j;
+    }
+    void pop(){
+        if(q.front().yy == i) q.pop_front();
+        ++i;
+    }
+    int get(){
+        return q.front().xx;
+    }
+}minq;
+
+struct DequeMax{
+    deque<pii> q;
+    int i=0, j=0;
+
+    void push(int x){
+        while(!q.empty() && q.back().xx <= x) q.pop_back();
+        q.push_back({x, j});
+        ++j;
+    }
+    void pop(){
+        if(q.front().yy == i) q.pop_front();
+        ++i;
+    }
+    int get(){
+        return q.front().xx;
+    }
+}maxq;
+
 //최근 m개의 최소값
 for(int i=0; i<n; ++i){
 	int a; cin >> a;
