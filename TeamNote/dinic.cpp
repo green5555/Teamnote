d89dabc4de31abc53a573cd3e8d75eaaf874e4cb
Green Jung <47073816+green5555@users.noreply.github.com>
2021-02-07 00:42:10 +0900

    change to good one
    author : https://github.com/koosaga/DeobureoMinkyuParty

--- TeamNote/dinic.cpp
+++ TeamNote/dinic.cpp
@@ -1,67 +1,53 @@
 //O(EV^2), 0-1 cap에서 min{O(EV^(2/3), O(E^(3/2))}
-const int MAX, INF; //MAX = 정점의 개수, INF = 최대 용량
-struct Dinic{
-	int level[MAX], work[MAX];
-	struct Edge{
-		int next, cap, flow=0;
-		Edge* rev;
-		Edge(){};
-		Edge(int _next, int _cap) : next(_next), cap(_cap) {};
-	};
-	vector<Edge*> adj[MAX];
-	inline void make_edge(int u, int v, int cap){
-		Edge *uv = new Edge(v, cap), *vu = new Edge(u, 0);
-		uv->rev = vu, vu->rev = uv;
-		adj[u].push_back(uv);
-		adj[v].push_back(vu);
-	}	
-	//싱크까지 도달 가능한 레벨 그래프를 생성 할 수 있는가?
-	bool bfs(int S, int E){
-		memset(level, -1, sizeof(level));
-		level[S] = 0;
-		queue<int> Q;
-		Q.push(S);
-		while(!Q.empty()){
-			int here = Q.front(); Q.pop();
-			for(auto &i : adj[here]){
-				int there = i->next;
-				//레벨이 정해지지 않고, 남은 용량이 있는 간선인가?
-				if(level[there] == -1 && i->cap >0){
-					level[there] = level[here]+1;
-					Q.push(there);
-				}
-			}
-		}
-		return (level[E] != -1);
-	}
-	//here에서 E에 도달할 때 까지 flow 만큼 흘려보내본다.
-	//싱크까지 얼마나 유량을 흘릴 수 있었는지 리턴한다.
-	int dfs(int here, int flow, int E){
-		if(here == E) return flow;
-		int size = adj[here].size();
-		for(int &i = work[here]; i<size; ++i){
-			int there = adj[here][i]->next, c = adj[here][i]->cap;
-			if(level[here]+1 == level[there] && c >0){
-				int nc = dfs(there, min(flow, c), E);
-				if(nc){
-					adj[here][i]->cap -= nc;
-					adj[here][i]->rev->cap += nc;
-					return nc;
-				}
-			}
-		}
-		return 0;
-	}
-	int maxflow(int S, int E){
-		int total = 0;
-		while(bfs(S,E)){
-			memset(work, false, sizeof(work));
-			while(true){
-				int flow = dfs(S, INF, E);
-				if(flow == 0) break;
-				total += flow;
-			}
-		}
-		return total;
-	}
-};
+const int MAX;
+struct Dinic {
+    struct edg {int pos, cap, rev;};
+    vector<edg> adj[MAX];
+    void clear() { for (int i = 0; i < MAX; i++) adj[i].clear(); }
+    void make_edge(int s, int e, int x) {
+        adj[s].push_back({e, x, (int) adj[e].size()});
+        adj[e].push_back({s, 0, (int) adj[s].size() - 1});
+    }
+    int dis[MAX], pnt[MAX];
+    bool bfs(int src, int sink) {
+        memset(dis, 0, sizeof(dis));
+        memset(pnt, 0, sizeof(pnt));
+        queue<int> que;
+        que.push(src);
+        dis[src] = 1;
+        while (!que.empty()) {
+            int x = que.front();
+            que.pop();
+            for (auto &e : adj[x]) {
+                if (e.cap > 0 && !dis[e.pos]) {
+                    dis[e.pos] = dis[x] + 1;
+                    que.push(e.pos);
+                }
+            }
+        }
+        return dis[sink] > 0;
+    }
+    int dfs(int x, int sink, int f) {
+        if (x == sink) return f;
+        for (; pnt[x] < adj[x].size(); pnt[x]++) {
+            edg e = adj[x][pnt[x]];
+            if (e.cap > 0 && dis[e.pos] == dis[x] + 1) {
+                int w = dfs(e.pos, sink, min(f, e.cap));
+                if (w) {
+                    adj[x][pnt[x]].cap -= w;
+                    adj[e.pos][e.rev].cap += w;
+                    return w;
+                }
+            }
+        }
+        return 0;
+    }
+    ll maxflow(int src, int sink) {
+        ll ret = 0;
+        while (bfs(src, sink)) {
+            int r;
+            while ((r = dfs(src, sink, 2e9))) ret += r;
+        }
+        return ret;
+    }
+}f;
