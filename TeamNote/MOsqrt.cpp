285ff7f843792b68be39af6e863dc93fbea35285
Green Jung <47073816+green5555@users.noreply.github.com>
2021-02-07 14:03:31 +0900

    Update MOsqrt.cpp

--- TeamNote/MOsqrt.cpp
+++ TeamNote/MOsqrt.cpp
@@ -47,3 +47,19 @@ void MOquery(){
 		ans[P.second] = ret;
 	}
 }
+
+//해밀턴 경로를 이용한 정렬 함수 (2^pow >= q)
+const int rotateDelta[4] = {3, 0, 0, 1};
+inline ll gilbertOrder(int x, int y, int pow=21, int rotate=0) {
+	if (pow == 0) return 0;
+	int hpow = 1 << (pow-1);
+	int seg = (x<hpow)?((y<hpow)?0:3):((y<hpow)?1:2);
+	seg = (seg + rotate) & 3;
+	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+	int nrot = (rotate + rotateDelta[seg]) & 3;
+	ll subSquareSize = 1LL << (2*pow - 2);
+	ll ans = seg * subSquareSize;
+	ll add = gilbertOrder(nx, ny, pow-1, nrot);
+	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+	return ans;
+}
