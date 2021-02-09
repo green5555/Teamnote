98c48fb5d33cbe85169deb78e3058fdb5b032fc2
Green Jung <47073816+green5555@users.noreply.github.com>
2021-02-09 13:52:23 +0900

    Update PST.cpp

--- TeamNote/PST.cpp
+++ TeamNote/PST.cpp
@@ -1,42 +1,46 @@
-const int MAX = 100100;
+const int MAX;
 struct PersistentSegmentTree{
-	struct Node{
-		int value = 0; //반드시 항등원
-		Node *left = 0, *right = 0;
-	};
-	vector<Node*> Tree;
-	void init(){
-		Tree.clear();
-		Tree.push_back(new Node());
-	}
-	void make_tree(int idx, int value){
-		Tree.push_back(make_tree(Tree.back(), idx, value, 0, MAX-1));
-	}
-	Node* make_tree(Node *prev, int idx, int value, int nodeL, int nodeR){
-		Node *now = new Node();
-		if(idx < nodeL || nodeR < idx) return prev;
-		if (nodeL == nodeR) {
-			now->value = prev->value + value;
-			return now;
-		}
-		int mid = (nodeL + nodeR) / 2;
-		if (!prev->left) 
-			prev->left = new Node(), prev->right = new Node();
-		now->left = make_tree(prev->left, idx, value, nodeL, mid);
-		now->right = make_tree(prev->right, idx, value, mid+1, nodeR);
-		now->value = now->left->value + now->right->value;
-		return now;
-	}
-	int query(int root_idx, int l, int r){
-		return query(Tree[root_idx], l, r, 0, MAX-1);
-	}
-	int query(Node *here, int l, int r, int nodeL, int nodeR){
-		if(here == 0) return 0;
-		if(nodeR < l || r < nodeL) return 0;
-		if(l <= nodeL && nodeR <= r) return here -> value;
-		int mid = (nodeL + nodeR)/2;
-		return query(here->left, l, r, nodeL, mid) + query(here->right, l, r, mid+1, nodeR);
-	}
+    struct Node{
+        int value = 0; //반드시 항등원
+        int lidx = -1, ridx = -1;
+    };
+    vector<int> root;
+    vector<Node> seg;
+
+    void init(){
+        root.clear(); seg.clear();
+        root.emplace_back(0);
+        seg.emplace_back(); //root
+    }
+
+    int make_tree(int prev, int idx, int value, int nl, int nr) {
+        int here = seg.size(); seg.emplace_back();
+        if(idx < nl || nr < idx) return prev;
+        if (nl == nr) {
+            seg[here].value = seg[prev].value + value;
+            return here;
+        }
+        int mid = (nl + nr) / 2;
+        if (seg[prev].lidx == -1) {
+            seg[prev].lidx = seg.size(); seg.emplace_back();
+            seg[prev].ridx = seg.size(); seg.emplace_back();
+        }
+        seg[here].lidx = make_tree(seg[prev].lidx, idx, value, nl, mid);
+        seg[here].ridx = make_tree(seg[prev].ridx, idx, value, mid+1, nr);
+        seg[here].value = seg[seg[here].lidx].value + seg[seg[here].ridx].value;
+        return here;
+    }
+    void make_tree(int idx, int value){
+        root.emplace_back(make_tree(root.back(), idx, value, 0, MAX-1));
+    }
+
+    int query(int idx, int l, int r, int nl, int nr){
+        if(idx == -1 || nr < l || r < nl) return 0;
+        if(l <= nl && nr <= r) return seg[idx].value;
+        int mid = (nl + nr)/2;
+        return query(seg[idx].lidx, l, r, nl, mid) + query(seg[idx].ridx, l, r, mid+1, nr);
+    }
+    int query(int root_idx, int l, int r){
+        return query(root[root_idx], l, r, 0, MAX-1);
+    }
 };
-for(int i=0; i<size_of_x; ++i)
-    PST.make_tree(p.yy, 1);
