6eccc3a1315345a4755849340f0cb06dde42207a
Green Jung <47073816+green5555@users.noreply.github.com>
2021-03-13 03:16:37 +0900

    Update dijk.cpp

--- TeamNote/dijk.cpp
+++ TeamNote/dijk.cpp
@@ -1,5 +1,5 @@
 const int MAX; const ll INF;
-vector<pll> adj[MAX]; //{weight, vertex}
+vector<pll> adj[MAX]; //{vertex, weight}
 void dijkstra(int start, vector<ll> &dist){
     dist = vector<ll>(MAX, INF);
     dist[start] = 0;
@@ -8,7 +8,7 @@ void dijkstra(int start, vector<ll> &dist){
     while(!q.empty()){
     	auto [cost, u] = q.top(); q.pop();
         if(cost > dist[u]) continue;
-        for(auto &[w, v] : adj[u]){
+        for(auto &[v, w] : adj[u]){ //{w,v}?
             ll ncost = cost + w;
             if(dist[v] > ncost){
                 q.emplace(ncost, v);
