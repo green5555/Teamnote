c4f30ebe3c5456393e45c2f817ed12ac91e5ae4f
Green Jung <47073816+green5555@users.noreply.github.com>
2021-08-29 20:40:20 +0900

    Update segTree.cpp

--- TeamNote/segTree.cpp
+++ TeamNote/segTree.cpp
@@ -1,17 +1,16 @@
-const int MAX; 
+const int MAX=0;
 struct SegmentTree{
     ll seg[MAX*4];
-    ll query(int l, int r, int no, int nl, int nr){
+    ll query(int l, int r, int no=1, int nl=0, int nr=MAX-1){
         if(r < nl || nr < l) return 0;
         if(l <= nl && nr <=r) return seg[no];
-        int mid = (nl + nr)/2;
-        return query(l, r, no*2, nl, mid) + query(l, r, no*2+1, mid+1, nr);
+        int m = (nl + nr)/2;
+        return (query(l, r, no*2, nl, m) + query(l, r, no*2+1, m+1, nr));
     }
-    ll update(int i, ll v, int no, int nl, int nr){
+    ll update(int i, ll v, int no=1, int nl=0, int nr=MAX-1){
         if(i<nl || nr<i) return seg[no];
         if(nl == nr) return seg[no] = v; // += v;
-        int mid = (nl + nr)/2;
-        return seg[no] = update(i,v,no*2,nl,mid) + update(i,v,no*2+1,mid+1,nr);
+        int m = (nl + nr)/2;
+        return seg[no] = (update(i,v,no*2,nl,m) + update(i,v,no*2+1,m+1,nr));
     }
 }s;
-
