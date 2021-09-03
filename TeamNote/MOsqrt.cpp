75fdbe052264287f482bf4a76c3f1744e9c74d49
Green Jung <47073816+green5555@users.noreply.github.com>
2021-09-03 21:50:37 +0900

    Update MOsqrt.cpp

--- TeamNote/MOsqrt.cpp
+++ TeamNote/MOsqrt.cpp
@@ -23,21 +23,19 @@ void MOquery(){
 	add(arr[0]);
 	for(auto &P : query){
 		int nL = P.first.first, nR = P.first.second;
-		//1. L을 nL로 바꿔주기.
-		if(L < nL){ //오른쪽으로 밀면서 [L, nL) 제거
-			for(int i=L; i<nL; ++i)
-				pop(arr[i]);
-		}
-		else{ //왼쪽으로 밀면서 추가 [nL, L) 추가
+		if(nL < L){//왼쪽으로 밀면서 추가 [nL, L) 추가
 			for(int i=L-1; i>=nL; --i)
 				add(arr[i]);
 		}
-		//2. R을 nR로 바꿔주기.
 		if(R < nR){//오른쪽으로 밀면서 (R, nR] 추가
 			for(int i=R+1; i<=nR; ++i)
 				add(arr[i]);
 		}
-		else{//왼쪽으로 밀면서 (nR, R] 제거
+		if(L < nL){//오른쪽으로 밀면서 [L, nL) 제거
+			for(int i=L; i<nL; ++i)
+				pop(arr[i]);
+		}
+		if(nR < R){//왼쪽으로 밀면서 (nR, R] 제거
 			for(int i=R; i>nR; --i)
 				pop(arr[i]);
 		}
