8a0dee506072f11a6b53443fb6c7c5bae9611e16
Green Jung <47073816+green5555@users.noreply.github.com>
2021-09-20 03:11:08 +0900

    Update fastIO.cpp

--- TeamNote/fastIO.cpp
+++ TeamNote/fastIO.cpp
@@ -1,26 +1,13 @@
-//FAST I/O
-namespace fio {
-	const int BSIZE = 524288;
-	char buffer[BSIZE];
-	int p = BSIZE;
-	inline char readChar() {
-		if(p == BSIZE) {
-			fread(buffer, 1, BSIZE, stdin);
-			p = 0;
-		}
-		return buffer[p++];
-	}
-	int readInt() {
-		char c = readChar();
-		while ((c < '0' || c > '9') && c != '-') {
-			c = readChar();
-		}
-		int ret = 0; bool neg = c == '-';
-		if (neg) c = readChar();
-		while (c >= '0' && c <= '9') {
-			ret = ret * 10 + c - '0';
-			c = readChar();
-		}
-		return neg ? -ret : ret;
-	}
+const int BSIZE = 524288; char buffer[BSIZE]; int p = BSIZE;
+inline char readc() {
+	if(p == BSIZE) { fread(buffer, 1, BSIZE, stdin); p = 0; } return buffer[p++];
 }
+int readi() {
+	char c = readc(); while ((c < '0' || c > '9') && c != '-') c = readc();
+	int ret = 0; bool neg = c == '-'; if (neg) c = readc();
+	while (c >= '0' && c <= '9') { ret = ret * 10 + c - '0'; c = readc(); }
+	return neg ? -ret : ret;
+}
+
+
+// don't forget sync_with_stdio && cin.tie
