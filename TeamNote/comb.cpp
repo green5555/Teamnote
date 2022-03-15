bfb6e868401962dfbe8764904dd0986cf9d024d9
Green Jung <47073816+green5555@users.noreply.github.com>
2022-03-15 20:07:04 +0900

    Update comb.cpp

--- TeamNote/comb.cpp
+++ TeamNote/comb.cpp
@@ -1,3 +1,22 @@
+const int MAX; //MAX = n의 최대값
+mint inv[MAX+1], fac[MAX+1], facInv[MAX+1];
+void combInit(){
+    fac[0] = 1;
+    for(int i=1; i<=MAX; ++i)
+        fac[i] = (fac[i-1]*i);
+    inv[1] = 1;
+    for(int i=2; i<=MAX; ++i)
+        inv[i] = (mod - mod/i) * inv[mod%i];
+    facInv[1] = 1;
+    for(int i=2; i<=MAX; ++i)
+        facInv[i] = (facInv[i-1]*inv[i]);
+}
+inline mint comb(int n, int r){
+	if(r == 0 || n == r) return 1;
+	return (((fac[n]*facInv[r]))*facInv[n-r]);
+}
+
+// Method1, with ll
 const int MAX, mod=1'000'000'007; //MAX = n의 최대값
 ll inv[MAX+1], fac[MAX+1], facInv[MAX+1];
 void combInit(){
