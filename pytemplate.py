abc1165830a051293cd31c7971cdb71c9b437dce
Green Jung <pasdfq@naver.com>
2022-06-18 22:41:45 +0900

    add pytemplate

---
+++ pytemplate.py
@@ -0,0 +1,4 @@
+import sys;input=lambda:sys.stdin.readline().strip('\n')
+mis=lambda:map(int, input().split())
+ii=lambda:int(input())
+
---
+++ pytemplate_tc.py
@@ -0,0 +1,12 @@
+import sys;input=lambda:sys.stdin.readline().strip('\n')
+mis=lambda:map(int, input().split())
+ii=lambda:int(input())
+
+def solve_tc() :
+    return
+
+if __name__ == "__main__" : 
+    for tc in range(ii()) :
+        print(f'Case #{tc+1}:', end=' ')
+        ret = solve_tc()
+        if ret != None : print(ret)
