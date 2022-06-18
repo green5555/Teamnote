917134dbd750a8c919d367bc1a8e6241f4f64144
Green Jung <pasdfq@naver.com>
2022-06-18 22:31:39 +0900

    delete archive

--- archive/2020 ICPC - Gr-Yee-n55/code/cycle.cpp
+++
@@ -1,8 +0,0 @@
-int visit[MAX];
-bool haveCycle(int here){
-	if(visit[here]) return visit[here] == -1;
-	visit[here] = -1;
-	for(int &there : adj[here]) if(haveCycle(there)) return true;
-	visit[here] = 1; return false;
-}
-memset(visit, -1, sizeof(visit));
