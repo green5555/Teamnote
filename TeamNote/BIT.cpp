eb4a72d41d4d4f3c4cf987d837c52976eb8636a9
Green Jung <pasdfq@naver.com>
2022-07-31 18:20:43 +0900

    update BIT

--- TeamNote/BIT.cpp
+++ TeamNote/BIT.cpp
@@ -1,23 +1,26 @@
-const int MAX;
+// idx : [0 ~ MAX)
+
+const int MAX = 1000000+1;
 struct BIT{
-    ll seg[MAX+1];
+    ll seg[MAX+2];
     void clear(){memset(seg, 0, sizeof(seg));}
-    ll psum(int idx){
+    ll psum(int idx){ ++idx;
         ll ret = 0;
         while(idx > 0){ ret += seg[idx], idx -= idx & -idx; }
         return ret;
     }
-    ll query(int l, int r){
-        ll ret = psum(r); if(l>0) ret -= psum(l-1);
+    ll query(int l, int r){ ++l, ++r;
+        ll ret = psum(r); if(l>1) ret -= psum(l-1);
         return ret;
     }
-    void update(int idx, ll a=1) {
+    void update(int idx, ll a=1) { ++idx;
         while (idx <= MAX){ seg[idx] += a; idx += idx & -idx; }
     }
-}
+}s;
 
-
-int kth(int k){
+// return : psum(x) = k를 만족하는 x
+//          k가 전체 합보다 크면 MAX 리턴.
+int kth(ll k){
     int ret = 0;
     for(int i=30; i>=0; --i){
         int pivot = ret + (1<<i);
@@ -26,5 +29,5 @@ int kth(int k){
             ret = pivot;
         }
     }
-    return ret+1;
+    return ret;
 }
