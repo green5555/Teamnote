2ba5c74443c7f6bc48cdf9145a5e72693c064808
Green Jung <pasdfq@naver.com>
2022-07-31 18:22:00 +0900

    update segTreeStruct

--- TeamNote/segTreeStruct.cpp
+++ TeamNote/segTreeStruct.cpp
@@ -1,26 +1,30 @@
+const int MAX;
+
 struct node{
+	// TODO
+	// 기본 생성자 = 항등원.
 };
 
-const int MAX;
 node seg[MAX*4];
 
 node merge(node l, node r){
-	//두 노드를 합치기
-	//기본 생성자 = 항등원
+	// TODO
 }
 
-node query(int l, int r, int node, int nodeL, int nodeR){
-	if(r < nodeL || nodeR < l) return node();
-	if(l <= nodeL && nodeR <=r) return seg[node]; 
-	int mid = (nodeL + nodeR)/2;
-	return merge(query(l, r, node*2, nodeL, mid), query(l, r, node*2+1, mid+1, nodeR));
+node query(int l, int r, int no, int nl, int nr){
+	if(r < nl || nr < l) return node();
+	if(l <= nl && nr <=r) return seg[no]; 
+	int mid = (nl + nr)/2;
+	return merge(query(l, r, no*2, nl, mid), query(l, r, no*2+1, mid+1, nr));
 }
 
-node update(int index, int value, int node, int nodeL, int nodeR){
-	if(index<nodeL || nodeR<index) return seg[node];
-	if(nodeL == nodeR)
-		return seg[node] = //value로 업데이트 된 값; 
-	int mid = (nodeL + nodeR)/2;
-	return seg[node] = merge(update(index,value,node*2,nodeL,mid),
-							update(index,value,node*2+1,mid+1,nodeR));
+void update(int i, node v, int no, int nl, int nr){
+	if(i<nl || nr<i) return;
+	if(nl == nr) {
+        seg[no] = v; //TODO : seg[no]에 v를 업데이트.
+        return;
+    }
+	int mid = (nl + nr)/2;
+    update(i,v,no*2,nl,mid), update(i,v,no*2+1,mid+1,nr);
+	seg[no] = merge(seg[no*2], seg[no*2+1]);
 }
