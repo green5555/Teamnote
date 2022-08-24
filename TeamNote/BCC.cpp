5fb6e410cfe7e72b10eb388c897a97deffd32fef
Green Jung <pasdfq@naver.com>
2022-08-25 02:00:29 +0900

    bridge를 bool 기반으로 변경

--- TeamNote/BCC.cpp
+++ TeamNote/BCC.cpp
@@ -1,12 +1,16 @@
 const int MAX = 100;
 struct{
+    vector<pii> edges;
     vector<pii> adj[MAX]; // { next vertex id, edge id }
+    
+    int is_cut[MAX]; // v is cut vertex if is_cut[v] > 0
+    vector<int> is_bridge; // e is bridge if is_bridege[e] == 1
+
+    int bcc_cnt;
+    vector<int> bcc_edges[MAX]; // list of edge ids in a bcc
+
     int up[MAX], visit[MAX], vtime;
     vector<int> stk;
-    int is_cut[MAX]; // v is cut vertex if is_cut[v] > 0
-    vector<int> bridge; // list of edge ids
-    vector<int> bcc_edges[MAX]; // list of edge ids in a bcc
-    int bcc_cnt;
     void dfs(int no, int pe) { // node, parent edge
         up[no] = visit[no] = ++vtime;
         int child = 0;
@@ -17,7 +21,7 @@ struct{
                 stk.push_back(eid);
                 ++child;
                 dfs(next, eid);
-                if (up[next] == visit[next]) bridge.push_back(eid);
+                if (up[next] == visit[next]) is_bridge[eid] = 1;
                 if (up[next] >= visit[no]) {
                     ++bcc_cnt;
                     do {
@@ -38,12 +42,17 @@ struct{
         if (pe == -1 && is_cut[no] == 1)
             is_cut[no] = 0;
     }
+    void add_edge(int x, int y) {
+        adj[x].emplace_back(y, edges.size());
+        adj[y].emplace_back(x, edges.size());
+        edges.emplace_back(x, y);
+    }
     // find BCCs & cut vertexs & bridges in undirected graph O(V+E)
     void get_bcc() {
         vtime = 0;
         memset(visit, 0, sizeof(visit));
         memset(is_cut, 0, sizeof(is_cut));
-        bridge.clear();
+        is_bridge.clear(); is_bridge.resize(edges.size()+1);
         for (int i = 0; i < MAX; ++i) bcc_edges[i].clear();
         bcc_cnt = 0;
         for (int i = 0; i < MAX; ++i) {
@@ -51,4 +60,4 @@ struct{
                 dfs(i, -1);
         }
     }
-}BCC;
+}b;
