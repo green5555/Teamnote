96f571867f9b72cedcfc838a948830c6931de958
Green Jung <pasdfq@naver.com>
2022-12-11 02:42:21 +0900

    mint 기반  코드 추가

--- TeamNote/hash.cpp
+++ TeamNote/hash.cpp
@@ -1,4 +1,26 @@
 const int MAX = 200;
+struct Hashing {
+    const ll base;
+    vector<mint> ppow;
+    constexpr Hashing(int base = 257): base(base) {
+        ppow.resize(MAX); ppow[0] = 1;
+        for (int i=1; i<MAX; ++i) ppow[i] = ppow[i-1] * base;
+    }
+    void hash(vector<mint> &h, const string &s) {
+        h.clear(); h.resize(s.size() + 1);
+        for (int i = 0; i < s.size(); ++i)
+            h[i + 1] = (h[i] + (s[i] + 1) * ppow[i]);
+    }
+    inline mint get(const vector<mint> &h, int l, int len) {
+        return (h[l + len] - h[l])* ppow[MAX-1-l];
+    }
+};
+
+
+
+// without mint
+
+const int MAX = 200;
 template<ll m = 1000000009, ll b = 257> struct Hashing {
     vector<ll> ppow;
     Hashing() {
