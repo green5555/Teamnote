2107ff994024a7632f28930408765c0b2d3e0a5f
Green Jung <pasdfq@naver.com>
2022-12-11 02:40:58 +0900

    변수 이름 수정 및 scc adj 만드는 함수 추가

--- TeamNote/SCC.cpp
+++ TeamNote/SCC.cpp
@@ -1,42 +1,59 @@
 const int MAX;
 struct StronglyConnectedComponent {
-    int discovered[MAX], sccn[MAX], cnt1, cnt2;
-    vector<int> adj[MAX];
+    int vst[MAX+1], vstn, sccn;
+    vector<int> adj[MAX+1];
     stack<int> s;
+
+    int scci[MAX+1]; // i in scc[i]
     vector<vector<int>> scc;
 
     int DFS(int here) {
-        discovered[here] = cnt1++;
-        int ret = discovered[here];
+        vst[here] = vstn++;
+        int ret = vst[here];
         s.push(here);
         for (int &there : adj[here]) {
-            if (discovered[there] == -1)
+            if (vst[there] == -1)
                 ret = min(ret, DFS(there));
-            else if (sccn[there] == -1)
-                ret = min(ret, discovered[there]);
+            else if (scci[there] == -1)
+                ret = min(ret, vst[there]);
         }
-        if (ret == discovered[here]) {
+        if (ret == vst[here]) {
             scc.emplace_back(vector<int>());
             auto &V = scc.back();
             while (true) {
                 int tmp = s.top();
                 s.pop();
-                sccn[tmp] = cnt2;
+                scci[tmp] = sccn;
                 V.push_back(tmp);
                 if (tmp == here) break;
             }
-            ++cnt2;
+            ++sccn;
         }
         return ret;
     }
 
-    void make_SCC(int V=MAX) {
+    void make_scc(int V=MAX) {
         scc.clear();
-        memset(discovered, -1, sizeof(discovered));
-        memset(sccn, -1, sizeof(sccn));
-        cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < V; ++i)
-            if (discovered[i] == -1)
+        memset(vst, -1, sizeof(vst));
+        memset(scci, -1, sizeof(scci));
+        vstn = 0, sccn = 0;
+        // TODO : check range
+        for (int i = 1; i <= V; ++i)
+            if (vst[i] == -1)
                 DFS(i);
+    }
+
+    vector<vector<int>> make_scc_adj() {
+        vector<vector<int>> sadj(scc.size());
+        for(int i=0; i<scc.size(); ++i) {
+            auto &A = sadj[i];
+            for(int x : scc[i])
+                for(int y : adj[x])
+                    if(scci[x] != scci[y])
+                        A.emplace_back(scci[y]);
+            sort(all(A));
+            A.erase(unique(all(A)), A.end());
+        }
+        return sadj;
     }
 }S;
